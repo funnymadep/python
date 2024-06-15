7fa3f9dbc4d029298674e1fdfb7254ee8eb05f64
hqlDebian12Pi <2454618038@qq.com>
2024-06-15 11:55:17 +0800

    first

---
+++ crawler/first.py
@@ -0,0 +1,8 @@
+import requests
+
+head = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' }
+response = requests.get("http://books.toscrape.com/", headers = head);
+if response.ok:
+    print(response.text);
+else:
+    print('requests error');
