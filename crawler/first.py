49dba6e035d2dfcac6d0f27c549c1532c7f0005b
hqlDebian12Pi <2454618038@qq.com>
2024-06-15 12:00:04 +0800

    first

--- crawler/first.py
+++ crawler/first.py
@@ -1,8 +1,8 @@
 import requests
 
 head = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' }
-response = requests.get("http://books.toscrape.com/", headers = head);
+response = requests.get("http://books.toscrape.com/", headers = head)
 if response.ok:
-    print(response.text);
+    print(response.text)
 else:
-    print('requests error');
+    print('requests error')
